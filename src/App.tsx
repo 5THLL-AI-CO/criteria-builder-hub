7a5bd46b2609f9cbc41970c4f02efec9c4d5f09a
5THLL-AI-CO <5thllaico@gmail.com>
2026-03-10 09:29:26 -0700

    feat: add /agente route with password-protected agent UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import IndexES from "./pages/IndexES";
 import NotFound from "./pages/NotFound";
+import AgentePage from "./pages/AgentePage";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/es" element={<IndexES />} />
+          <Route path="/agente" element={<AgentePage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
